f1818a8b088183335ca3bb6a16f97f69df812a6b
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-12-18 22:42:20 +0530

    Initialize tailwind CSS and set color's design tokens

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { useLayoutEffect } from "react";
 import { AppRoutes } from "./routes";
 
 function App() {
+  useLayoutEffect(() => {
+    const root = document.documentElement;
+    const vh = Math.max(root.clientHeight || 0, window.innerHeight || 0);
+    root.style.setProperty("--app-root-winh", `${vh}px`);
+  }, []);
+
   return <AppRoutes />;
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,69 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      borderColor: {
+        primary: {
+          50: "#DFE5EE",
+          100: "#DFEAF2",
+          200: "#E6EFF5",
+          900: "#343C6A",
+        },
+        gray: {
+          50: "#F3F3F5",
+          100: "#F4F5F7",
+        },
+      },
+      textColor: {
+        light: "#FFFFFF",
+        primary: {
+          50: "#8BA3CB",
+          100: "#718EBF",
+          600: "#396AFF",
+          900: "#343C6A",
+        },
+        gray: {
+          200: "#B1B1B1",
+          950: "#232323",
+        },
+      },
+      backgroundColor: {
+        light: "#FFFFFF",
+        gray: {
+          50: "#F5F7FA",
+          100: "#F3F3F5",
+          200: "#EDF1F7",
+          600: "#9199AF",
+        },
+      },
+      colors: {
+        light: "#FFFFFF",
+        primary: {
+          100: "#E7EDFF",
+          600: "#396AFF",
+          900: "#343C6A",
+        },
+        secondary: {
+          50: "#DCFAF8",
+          300: "#16DBCC",
+        },
+        gray: {
+          950: "#232323",
+        },
+        error: {
+          500: "#FF4B4A",
+        },
+        warning: {
+          50: "#FFF5D9",
+          300: "#FFBB38",
+          500: "#FC7900",
+        },
+        success: {
+          400: "#41D4A8",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
