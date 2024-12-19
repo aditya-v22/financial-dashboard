f22a391aeed494e58f0e5ea35667e62e521495ce
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-12-19 18:36:02 +0530

    Add keys in constants

---
+++ src/constanst/keys.ts
@@ -0,0 +1,10 @@
+export const KEYS = {
+  TAB: 'Tab',
+  ENTER: 'Enter',
+  SPACE: ' ',
+  ESCAPE: 'Escape',
+  ARROW_LEFT: 'ArrowLeft',
+  ARROW_RIGHT: 'ArrowRight',
+  ARROW_UP: 'ArrowUp',
+  ARROW_DOWN: 'ArrowDown',
+};
