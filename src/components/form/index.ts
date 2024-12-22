de82859906e5ce7276f7cd26c4390cdcdfb2d859
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-12-22 13:10:04 +0530

    Add form input field

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,2 +1,5 @@
 export { DatePickerField } from './DatePickerField';
 export type { DatePickerFieldProps } from './DatePickerField';
+
+export { InputField } from './InputField';
+export type { InputFieldProps } from './InputField';
