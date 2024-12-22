b751b487cb045c86610745dacd7bcc6bcaa4b40d
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-12-22 15:52:34 +0530

    Add chip icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,7 @@
 export { ArrowIcon } from './Arrow';
 export { PencilIcon } from './Pencil';
+export { ChipIcon } from './Chip';
 
 export type { ArrowIconProps } from './Arrow';
 export type { PencilIconProps } from './Pencil';
+export type { ChipIconProps } from './Chip';
