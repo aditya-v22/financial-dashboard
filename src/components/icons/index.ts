f862ff90515b7c3df6f95b1ea0917d3d50343c28
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-12-22 23:25:27 +0530

    Add send icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -4,6 +4,7 @@ export { ChipIcon } from './Chip';
 export { CardsIcon } from './Cards';
 export { PaypalIcon } from './Paypal';
 export { MoneyWithRingIcon } from './Money';
+export { SendIcon } from './Send';
 
 export type { ArrowIconProps } from './Arrow';
 export type { PencilIconProps } from './Pencil';
@@ -11,3 +12,4 @@ export type { ChipIconProps } from './Chip';
 export type { CardsIconProps } from './Cards';
 export type { PaypalIconProps } from './Paypal';
 export type { MoneyWithRingIconProps } from './Money';
+export type { SendIconProps } from './Send';
