dcaa2ea0ae46fa3a7c6d54536f565a7eb6ad6060
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-12-22 18:54:42 +0530

    Add card, paypal, money icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,7 +1,13 @@
 export { ArrowIcon } from './Arrow';
 export { PencilIcon } from './Pencil';
 export { ChipIcon } from './Chip';
+export { CardsIcon } from './Cards';
+export { PaypalIcon } from './Paypal';
+export { MoneyWithRingIcon } from './Money';
 
 export type { ArrowIconProps } from './Arrow';
 export type { PencilIconProps } from './Pencil';
 export type { ChipIconProps } from './Chip';
+export type { CardsIconProps } from './Cards';
+export type { PaypalIconProps } from './Paypal';
+export type { MoneyWithRingIconProps } from './Money';
