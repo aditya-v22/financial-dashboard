82ab04707d50c3f3ac0afd31c251dbef39884225
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-12-22 13:16:53 +0530

    Add setting page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Setting from '../components/settings';
 
 const Settings: React.FC = () => {
-  return <Setting />;
+  return (
+    <div className='mx-10 my-[30px]'>
+      <Setting />
+    </div>
+  );
 };
 
 export default Settings;
