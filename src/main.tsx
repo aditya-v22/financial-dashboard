429ada7320fcd8dab6995230a25eb063e254deba
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-12-22 13:19:08 +0530

    Add log message

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,10 @@ import { BrowserRouter } from 'react-router-dom';
 const root = createRoot(document.getElementById('root')!, {
   // Catch errors in any components below and re-render with error message
   onCaughtError(error, errorInfo) {
-    console.error(error, errorInfo);
+    console.error('Caught error at root: ', error, errorInfo);
   },
   onUncaughtError(error, errorInfo) {
-    console.error(error, errorInfo);
+    console.error('Caught (Uncaught) error at root: ', error, errorInfo);
   },
 });
 
