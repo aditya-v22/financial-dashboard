3fcbec73a368c32071414c345190159d1a907f3d
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-12-22 13:15:55 +0530

    Add Preferences Setting component

---
+++ src/components/preferences-setting/PreferencesSetting.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PreferencesSetting: React.FC = () => {
+  return <div className='pt-8 pl-8 w-full'>Preferences Setting</div>;
+};
+
+export { PreferencesSetting };
