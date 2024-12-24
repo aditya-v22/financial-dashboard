6a780a21c5fb3f76ae8bd2c677fc87e0a260b4c7
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-12-24 18:39:59 +0530

    Update icon attribute

--- src/components/icons/X.tsx
+++ src/components/icons/X.tsx
@@ -11,9 +11,9 @@ const XIcon: React.FC<XIconProps> = ({ className, ...props }) => {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       className={cn('inline-block', className)}
       {...props}
     >
